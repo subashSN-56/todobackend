0d1a43bc84d49f4cab53f7a447fc6663ba4a0d10
subash <subash85248@gmail.com>
2025-04-18 10:07:21 +0530

    Design creative todo UI with CSS glassmorphism and responsive layout

---
+++ server.js
@@ -0,0 +1,124 @@
+
+const mongoose = require("mongoose");
+
+const express = require('express');
+const cors = require('cors'); // Importing CORS once
+
+
+ // Using CORS middleware
+
+
+
+const app = express();
+const port = 8000;
+
+
+app.use(express.json());
+app.use(cors());
+// 💡 Serve frontend files from 'public' folder
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// TTpiYGJsTE75NMKd
+mongoose.connect('mongodb+srv://subash56:TTpiYGJsTE75NMKd@cluster56.hphvs.mongodb.net/todo?retryWrites=true&w=majority&appName=Cluster56').then(()=>{
+    console.log("DB connected .......");
+}).catch((err)=>{
+    console.log(err);
+    
+});
+
+const todoschema = new mongoose.Schema({
+    
+    title: {
+        required:true,
+        type:String
+    },
+    description:String
+})
+
+
+const todomodel = mongoose.model('Todo',todoschema);
+
+//send data.............................................................................................
+
+app.post("/todos",async (req, res) => {
+    const { title, description } = req.body;
+
+    // const newtodo = {
+    //     id: todos.length + 1,
+    //     title,
+    //     description,
+    // };
+    // todos.push(newtodo);
+    // console.log(todos);
+    try {
+        const newTodossave = new todomodel({title,description});
+        await newTodossave.save();
+        res.status(201).json(newTodossave);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({massage:error.massage})      
+    }  
+});
+//update mothod ..........................................................................
+app.put("/todos/:id", async (req, res) => {
+    try {
+      const { title, description } = req.body;
+      const id = req.params.id;
+  
+      const updated = await todomodel.findByIdAndUpdate(
+        id,
+        { title, description },
+        { new: true }
+      );
+  
+      if (!updated) {
+        return res.status(404).json({ message: "Todo not found" });
+      }
+  
+      res.json(updated);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
+
+//delete mothed .......................................................
+app.delete("/todos/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+  
+      const deleted = await todomodel.findByIdAndDelete(id);
+  
+      if (!deleted) {
+        return res.status(404).json({ message: "Todo not found" });
+      }
+  
+      res.json({ message: "Todo deleted", todo: deleted });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
+
+//get mothod ............................................................................
+app.get('/todos',async (req, res) => {
+    try {
+        const todoss= await todomodel.find();
+        res.json(todoss);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({massage:error.massage}) ;
+
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`server is ready on port ${port}`);
+});
+
